505702a937c097e94fa9ddef5c8235a522af619c
kjh <kjh4940@naver.com>
2024-02-12 13:01:35 +0900

    no message

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -9,12 +9,13 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
-//        basePackages = "hello.core.member",
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
+/*
     @Bean(name = "memoryMemberRepository")
     MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
+*/
 }
